4aa4a068c1286280435a92154c5364206e569dbf
Alex <alexandrogonsan@outlook.com>
2024-07-21 16:34:51 -0400

    start js_compiler.py

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -6,48 +6,56 @@ var base = window.location.protocol + "//" + domain
 if (window.location.port) {
     base += ':' + window.location.port;
 }
-const colorsAndIcons = ['btn-danger','btn-primary','btn-success','btn-warning','btn-info','btn-dark'];
-// Função para popular os botões com cliques do banco de dados ao carregar a página
 function populateButtons() {
-    const url = `${base}/buttons/get.php?user=${getQueryParam('user')}`;
+    let url = `${base}/buttons/get.php?user=${getQueryParam('user')}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
             if (Array.isArray(data)) {
-                const container = document.getElementById('buttonsContainer');
+                document.getElementById('buttonCount').value = data.length;
+                let 
+                container = document.getElementById('buttonsContainer');
+                container.innerHTML = '';
                 // Estilo para garantir que os botões fiquem em linha e centralizados
                 container.style.display = 'flex';
                 container.style.flexWrap = 'wrap';
                 container.style.justifyContent = 'center';
                 container.style.gap = '10px'; // Espaçamento entre os botões
-
-                data.forEach(buttonData => {
-                    const { button, color, clicks } = buttonData;
-                    const aux_button = document.createElement('button');
-                    aux_button.type = 'button';
-                    aux_button.className = `btn ${color} me-2`;
-                    aux_button.innerHTML = `Botão <span id="counter${button}" class="badge bg-secondary">${clicks}</span>`;
-                    aux_button.onclick = function() {
-                        let clicks = incrementCounter(button);
-                        sendPostRequest(`${getQueryParam('user')}`, button, clicks, color);
-                    };
-
-                    const colorSelect = document.createElement('select');
-                    colorSelect.className = 'form-select d-inline-block w-auto me-2';
-                    colorSelect.onchange = function() {
-                        aux_button.className = `btn ${this.value} me-2`;
-                        sendPostRequest(`${getQueryParam('user')}`, button, clicks, this.value);
-                    };
-
-                    colorsAndIcons.forEach((color) => {
-                        const option = document.createElement('option');
-                        option.value = color;
-                        option.text = color.replace('btn-', '').toUpperCase();
-                        colorSelect.appendChild(option);
-                    });
-
-                    container.appendChild(colorSelect);
-                    container.appendChild(aux_button);
+                    data.forEach(buttonData => {
+                    let{
+                    button, 
+                        color, 
+                            clicks } = buttonData;
+                                let 
+                                aux_button = document.createElement('button');
+                                aux_button.type = 'button';
+                                aux_button.className = `btn ${color} me-2`;
+                                aux_button.innerHTML = `Botão <span id="counter${button}" class="badge bg-secondary">${clicks}</span>`;
+                                aux_button.onclick = function() {
+                                    let 
+                                    clicks = incrementCounter(button);
+                                            sendPostRequest(`${getQueryParam('user')}`, 
+                    button, 
+                                    clicks, 
+                        color);     };
+                                        let
+                                        colorSelect = document.createElement('select');
+                                        colorSelect.className = 'form-select d-inline-block w-auto me-2';
+                                        colorSelect.onchange = function() {
+                                aux_button.className = `btn ${this.value} me-2`;
+                                            sendPostRequest(`${getQueryParam('user')}`, button, clicks, this.value);
+                                            };
+                                            ['btn-danger','btn-primary','btn-success','btn-warning','btn-info','btn-dark']
+                                            .forEach((
+                                            color_option) => {
+                                                let 
+                                                option = document.createElement('option');
+                                                option.selected = (color === color_option);
+                                                option.value = color_option;
+                                                option.text = color_option.replace('btn-', '').toUpperCase();
+                                        colorSelect.appendChild(option);});
+                container.appendChild(colorSelect);
+                container.appendChild(aux_button);
                 });
             } else {
                 console.error('Unexpected response format:', data);
@@ -55,65 +63,50 @@ function populateButtons() {
         })
         .catch(error => console.error('Error fetching buttons data:', error));
 }
-function generateUserId() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-}
 function getQueryParam(param) {
-    const urlParams = new URLSearchParams(window.location.search);
+    let urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
 }
-// Função para obter uma cor e ícone únicos que não tenham sido usados anteriormente
-function getUniqueColorAndIcon(usedColors) {
-    let index = 0;
-    while (true) {
-        const { color, icon } = colorsAndIcons[index];
-        if (!usedColors.has(color)) {
-            usedColors.add(color);
-            return { color, icon };
-        }
-        index = (index + 1) % colorsAndIcons.length;
-    }
-}
-
 function getRandomColor() {
-    const colors = ['btn-danger', 'btn-primary', 'btn-success', 'btn-warning', 'btn-info', 'btn-dark'];
     return colors[Math.floor(Math.random() * colors.length)];
 }
-
+function getRandomHex() {
+    let letters = '0123456789ABCDEF';
+    let hex = '';
+    for (let i = 0; i < 6; i++) {
+        hex += letters[Math.floor(Math.random() * 16)];
+    }
+    return hex;
+}
 function createButtons() {
-    const buttonCount = document.getElementById('buttonCount').value;
-    const container = document.getElementById('buttonsContainer');
-    container.innerHTML = '';
-
-    const usedColors = new Set(); // Conjunto para rastrear cores já usadas
-
-    for (let i = 0; i < buttonCount; i++) {
-        // const buttonColor = getRandomColor();
-        const { color, icon } = getUniqueColorAndIcon(usedColors); // Obter cor e ícone únicos
-        const buttonId = `button${i}`;
-
-        const button = document.createElement('button');
+    let buttonCount = document.getElementById('buttonCount').value;
+    let container = document.getElementById('buttonsContainer');
+    if (container.innerHTML == '') { return }
+    for (let i = buttonCount; i < buttonCount + 1; i++) {
+        let color = getRandomColor();
+        let user = getRandomHex();
+        let buttonId = `button${i}`;
+        let button = document.createElement('button');
         button.type = 'button';
         button.className = `btn ${color} me-2`;
         button.id = buttonId;
-        button.innerHTML = `<i class="fas ${icon}"></i> Botão ${i + 1} <span id="counter${i}" class="badge bg-secondary">0</span>`;
-        // button.innerHTML = `Botão ${i + 1} <span id="counter${i}" class="badge bg-secondary">0</span>`;
+        button.innerHTML = `Botão ${i + 1} <span id="counter${i}" class="badge bg-secondary">0</span>`;
         button.onclick = function() {
-            incrementCounter(i);
-            sendPostRequest(buttonId, i);
+            let clicks = incrementCounter(i);
+            sendPostRequest(user, button, clicks, color);
         };
 
-        const colorSelect = document.createElement('select');
+        let colorSelect = document.createElement('select');
         colorSelect.className = 'form-select d-inline-block w-auto me-2';
         colorSelect.onchange = function() {
             button.className = `btn ${this.value} me-2`;
         };
 
-        const colors = ['btn-danger', 'btn-primary', 'btn-success', 'btn-warning', 'btn-info', 'btn-dark'];
-        colors.forEach(color => {
-            const option = document.createElement('option');
-            option.value = color;
-            option.text = color.replace('btn-', '').toUpperCase();
+        colors.forEach(color_option => {
+            let option = document.createElement('option');
+            option.selected = (color === color_option);
+            option.value = color_option;
+            option.text = color_option.replace('btn-', '').toUpperCase();
             colorSelect.appendChild(option);
         });
 
@@ -122,15 +115,15 @@ function createButtons() {
     }
 }
 function incrementCounter(buttonIndex) {
-    const counter = document.getElementById(`counter${buttonIndex}`);
+    let counter = document.getElementById(`counter${buttonIndex}`);
     counter.textContent = parseInt(counter.textContent) + 1;
     return counter.textContent;
 }
-    // const userId = localStorage.getItem('userId') || generateUserId(); // Obtém ou gera o ID do usuário
-    // localStorage.setItem('userId', userId); // Salva o ID do usuário no localStorage
+// let userId = localStorage.getItem('userId') || generateUserId(); // Obtém ou gera o ID do usuário
+// localStorage.setItem('userId', userId); // Salva o ID do usuário no localStorage
 function sendPostRequest(user, button, clicks, color) {
-    const url = '/buttons/post.php';
-    const data = { user: user, button: button, clicks: clicks, color: color };
+    let url = '/buttons/post.php';
+    let data = { user: user, button: button, clicks: clicks, color: color };
     console.log(data);
     fetch(url, {
         method: 'POST',
@@ -141,6 +134,6 @@ function sendPostRequest(user, button, clicks, color) {
     .catch((error) => console.error('Error:', error));
 }
 document.addEventListener('DOMContentLoaded', function() {
-    // createButtons(); // Chamada inicial para criar os botões com o valor padrão
+    createButtons(); // Chamada inicial para criar os botões com o valor padrão
     populateButtons();
 });
