1c08381c338645967ebbd90c3de81779ae6662b6
Alex <alexandrogonsan@outlook.com>
2024-08-18 17:59:22 -0400

    qwe

--- res/dev/py/a1.py
+++ res/dev/py/a1.py
@@ -11,6 +11,7 @@ def remove_script_from_file(file_path):
         content = file.read()
 
     script_tag = '&lt;script src="/scripts/redirect-to-lang.js" id="change-lang-autos"&gt;&lt;/script&gt;'
+                 '&lt;script src="/scripts/redirect-to-lang.js" id="লেং-অটোসমূহ সলনি কৰক"&gt;&lt;/script&gt;'
     if script_tag in content:
         content = content.replace(script_tag, '')
         with open(file_path, 'w', encoding='utf-8') as file:
